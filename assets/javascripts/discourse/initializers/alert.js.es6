17e5d64b7afea9059360339154aa7c3f18cb76fb
david.verdin <david.verdin@zenimax.com>
2016-09-29 16:48:12 -0400

    Simple plugin outline: DVerdin

---
+++ assets/javascripts/discourse/initializers/alert.js.es6
@@ -0,0 +1,6 @@
+export default {
+    name: 'steam-oauth-log',
+    initialize() {
+        console.log('Look I ran!');
+    }
+};
---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: steam-oauth
+# about: A super simple plugin to demonstrate how plugins work
+# version: 0.0.1
+# authors: David Verdin
+
+enabled_site_setting :steam_oauth_enabled
